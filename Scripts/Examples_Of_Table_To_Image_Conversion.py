e43793d75f928343f7f5efad6e33371bf866ff2e
zhuyitan <generegu@gmail.com>
2024-01-28 12:50:13 -0600

    Update Examples_Of_Table_To_Image_Conversion.py

--- Scripts/Examples_Of_Table_To_Image_Conversion.py
+++ Scripts/Examples_Of_Table_To_Image_Conversion.py
@@ -8,7 +8,7 @@ num_row = 30    # Number of pixel rows in image representation
 num_col = 30    # Number of pixel columns in image representation
 num = num_row * num_col # Number of features to be included for analysis, which is also the total number of pixels in image representation
 save_image_size = 3 # Size of pictures (in inches) saved during the execution of IGTD algorithm.
-max_step = 10000    # The maximum number of iterations to run the IGTD algorithm, if it does not converge.
+max_step = 30000    # The maximum number of iterations to run the IGTD algorithm, if it does not converge.
 val_step = 300  # The number of iterations for determining algorithm convergence. If the error reduction rate
                 # is smaller than a pre-set threshold for val_step itertions, the algorithm converges.
 
